56d8e75950c58be70f73d9a71f49793cba262dbc
Developer <poonacha.avinash@gmail.com>
2021-11-05 17:13:04 -0400

    String and property bindings
    Signed-off-by: Developer <poonacha.avinash@gmail.com>

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,14 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
  selector: 'app-servers',
 // selector: '.app-servers',
-  template: `
+templateUrl : './servers.component.html',
+  /* template: `
   <app-server></app-server>
-  <app-server></app-server>`,
+  <app-server></app-server>`, */
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+
+  setTimeout(() => {
+  this.allowNewServer = true;
+  },2000);
+  }
 
   ngOnInit(): void {
   }
